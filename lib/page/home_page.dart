b1be7639a0ee65cd9c329f69b7c5197d784b9aea
DYL521 <1016068291@qq.com>
2020-07-19 17:55:46 +0800

    add 3层楼的商品数据～～

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -14,19 +14,16 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-
 /**
  *  为了保持页面的状态， 就是从另外一个tab 跳转回来的时候，不必再加载数据， 使用混入 AutomaticKeepAliveClientMixin
  */
-class _HomePageState extends State<HomePage>  with AutomaticKeepAliveClientMixin{
+class _HomePageState extends State<HomePage>
+    with AutomaticKeepAliveClientMixin {
   String homePageContent = "正在获取数据";
-
 
   @override
   // TODO: implement wantKeepAlive 保持页面状态
   bool get wantKeepAlive => true;
-
-
 
   @override
   void initState() {
@@ -69,9 +66,18 @@ class _HomePageState extends State<HomePage>  with AutomaticKeepAliveClientMixin
             String leadImage = data['data']["shopinfo"]['leadImage'];
             String leadPhone = data['data']["shopinfo"]['leadPhone'];
 
-            //
+            // 推荐商品
             List<Map> recommonendList =
                 (data['data']["recommonend"] as List).cast();
+
+            // 楼层数据
+            String floor1Title = data['data']["floor1Pic"]['PiCTURE_ADDRESS'];
+            String floor2Title = data['data']["floor2Pic"]['PiCTURE_ADDRESS'];
+            String floor3Title = data['data']["floor3Pic"]['PiCTURE_ADDRESS'];
+            // 楼层的商品信息
+            List<Map> floor1 = (data['data']['floor1'] as List).cast();
+            List<Map> floor2 = (data['data']['floor2'] as List).cast();
+            List<Map> floor3 = (data['data']['floor3'] as List).cast();
 
             return SingleChildScrollView(
               child: Column(
@@ -81,6 +87,16 @@ class _HomePageState extends State<HomePage>  with AutomaticKeepAliveClientMixin
                   AdBanner(adPicture),
                   LeaderPhone(leadImage, leadPhone),
                   Recommend(recommonendList),
+
+                  //3层商品数据
+                  FloorTitle(floor1Title),
+                  FloorContent(floor1),
+
+                  FloorTitle(floor2Title),
+                  FloorContent(floor2),
+
+                  FloorTitle(floor3Title),
+                  FloorContent(floor3),
                 ],
               ),
             );
@@ -94,8 +110,6 @@ class _HomePageState extends State<HomePage>  with AutomaticKeepAliveClientMixin
       ), // 异步请求动态
     );
   }
-
-
 }
 
 // 首页轮播组件
@@ -300,6 +314,81 @@ class Recommend extends StatelessWidget {
       margin: EdgeInsets.only(top: 10),
       child: Column(
         children: [_titleWidget(), _recommedList()],
+      ),
+    );
+  }
+}
+
+// 楼层标题
+class FloorTitle extends StatelessWidget {
+  final String picture_address;
+
+  FloorTitle(this.picture_address);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Image.network(picture_address),
+    );
+  }
+}
+
+// 楼层商品列表内容
+class FloorContent extends StatelessWidget {
+  final List floorGoodsList;
+
+  FloorContent(this.floorGoodsList);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          _firstRow(),
+          _otherGoods(),
+        ],
+      ),
+    );
+  }
+
+  /**
+   * 第一行
+   */
+  Widget _firstRow() {
+    return Row(
+      children: <Widget>[
+        _goodsItem(floorGoodsList[1]),
+        Column(
+          children: <Widget>[
+            _goodsItem(floorGoodsList[1]),
+            _goodsItem(floorGoodsList[2]),
+          ],
+        )
+      ],
+    );
+  }
+
+  /**
+   *  第二行
+   */
+  Widget _otherGoods() {
+    return Row(
+      children: <Widget>[
+        _goodsItem(floorGoodsList[1]),
+        _goodsItem(floorGoodsList[2]),
+      ],
+    );
+  }
+
+  Widget _goodsItem(Map goods) {
+    return Container(
+      width: ScreenUtil().setHeight(370),
+      child: InkWell(
+        onTap: () {
+          print("点击率楼层商品");
+        },
+        child: Image.network(goods["image"]),
       ),
     );
   }

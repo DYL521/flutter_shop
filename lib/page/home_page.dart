882eb2b715561a864fef64899774d797afc38364
DYL521 <1016068291@qq.com>
2020-07-19 08:52:37 +0800

    首页轮播组件添加～～

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,19 +11,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  String homePageContent  = "正在获取数据";
-
+  String homePageContent = "正在获取数据";
 
   @override
   void initState() {
     // TODO: implement initState
     // 获取主页数据
-    getHomePageContent().then((val){
+    getHomePageContent().then((val) {
       setState(() {
         homePageContent = val.toString();
       });
-
     });
     super.initState();
   }
@@ -28,9 +28,51 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("百姓生活家"),),
-      body: SingleChildScrollView(
-      child: Text(homePageContent),
+      appBar: AppBar(
+        title: Text("百姓生活家"),
+      ),
+      body: FutureBuilder(
+        future: getHomePageContent(), //异步方法
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            // 有无数据
+            var data = json.decode(snapshot.data.toString());
+            List<Map> swiperDataList =
+                (data['data']['slides'] as List).cast(); // 解析成List
+            return Column(
+              children: <Widget>[
+                WpiperDiy(swiperDataList),
+              ],
+            );
+          } else { //没有数据
+            return Center(
+              child: Text("加载中..."),
+            );
+          }
+        },
+      ), // 异步请求动态
+    );
+  }
+}
+
+// 首页轮播组件
+class WpiperDiy extends StatelessWidget {
+  final List swiperDateList;
+
+  // 构造方法
+  WpiperDiy(this.swiperDateList);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 333,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network("${swiperDateList[index]['image']}",fit: BoxFit.fill,);
+        },
+        itemCount: swiperDateList.length,
+        pagination: new SwiperPagination(),
+        autoplay: true, // 自动播放
       ),
     );
   }
